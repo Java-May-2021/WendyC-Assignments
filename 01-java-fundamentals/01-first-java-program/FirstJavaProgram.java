e1fcb92f14b2d60c871b3913b2867af12a45e5e8
Wendy Wu Chiang <wendywu0122@gmail.com>
2021-05-17 01:32:39 -0700

    first

--- 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -9,8 +9,8 @@
 
 public class FirstJavaProgram{
     public static void main(String[] args){
-        System.out.println("My name is Coding Dojo");
-        System.out.println("I am 100 years old");
-        System.out.println("My hometown is Berbank,CA");
+        System.out.println("My name is Wendy Wu.");
+        System.out.println("I am 32 years old.");
+        System.out.println("My hometown is Nanping, China.");
     }
 }
